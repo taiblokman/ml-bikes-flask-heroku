1043bed00c21eab352e71385cd5446692125c6f2
taib lokman <taiblokman@gmail.com>
2022-03-27 18:48:17 +0800

    added upload/download bulk prediction

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,6 @@
+import glob
 import os
+import time
 import flask
 import pickle
 import pandas as pd
@@ -70,11 +72,19 @@ def main():
 
 # Get the uploaded files
 @app.route("/upload", methods=['GET','POST'])
-def uploadFiles():
+def upload():
+    # filenames = os.listdir('static/files')
+    # list files in directory but remove hidden files
+    # filenames = glob.glob(app.config['UPLOAD_FOLDER']+'/*')
+    filenames = get_files_in_dir(app.config['UPLOAD_FOLDER'])    
+    # get file time since epoch
+    print(filenames)
+    files_w_dates = populate_files_w_dates(filenames)
+
     if flask.request.method == 'GET':
-        # return the upload page
-        return(flask.render_template('upload.html'))
+        return(flask.render_template('upload.html', files=files_w_dates))
       # get the uploaded file
+
     if flask.request.method == 'POST':  
       uploaded_file = flask.request.files['file']
       if uploaded_file.filename != '':
@@ -83,10 +93,10 @@ def uploadFiles():
         uploaded_file.save(file_path)
         # parse the CSV
         # parseCSV(file_path)
-        out_file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'predicted.csv')
+        filename_no_ext = uploaded_file.filename.rsplit('.', 1)[0]
+        out_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename_no_ext+'_prediction.csv')
         process(file_path,out_file_path)
-
-      return flask.redirect(flask.url_for('main'))
+      return flask.redirect(flask.url_for('upload'))
 
 # parse the CSV file
 # note: for now we're expecting a correctly form file
@@ -105,8 +115,26 @@ def parseCSV(filePath):
     #          mydb.commit()
     #          print(i,row['first_name'],row['last_name'],row['address'],row['street'],row['state'],row['zip'])
 
+def get_files_in_dir(dir):
+    return [file for file in os.listdir(dir) if not file.startswith('.')]
+
+def populate_files_w_dates(filenames):
+    all_files = []
+    for fls in filenames:
+        # print(fls)
+        modTimesinceEpoc = os.path.getmtime(app.config['UPLOAD_FOLDER']+'/'+fls)
+        # Convert seconds since epoch to readable timestamp
+        ep_time = str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(modTimesinceEpoc)))
+        # all dates to list of files
+        all_files.append([fls,ep_time])        
+        # sort by dates using lambda/tuple
+        # https://stackoverflow.com/questions/65790036/python-sort-list-of-lists-by-not-one-but-two-criteria
+        sorted_all_files = sorted(all_files, key=lambda x:(str(x[1]),x[0]))
+    return sorted_all_files  
+
 def process(inPath, outPath):
   # read input file
+    print(inPath)
     col_names = ['temperature', 'humidity', 'windspeed']
     # Use Pandas to parse the CSV file
     # input_df = pd.read_csv(inPath,names=col_names, header=None)
@@ -123,6 +151,24 @@ def process(inPath, outPath):
     # output_df.to_csv(outPath, index=False)
     output_df = input_df
     output_df.to_csv(outPath, index=False)
+
+### download files
+# https://stackoverflow.com/questions/67373819/python-flask-app-which-returns-list-of-files
+@app.route('/download')
+def files():
+    # filenames = os.listdir('static/files')
+    # filenames = glob.glob(app.config['UPLOAD_FOLDER'], '*')
+    filenames = get_files_in_dir(app.config['UPLOAD_FOLDER'])
+    files_w_dates = populate_files_w_dates(filenames)
+    return flask.render_template('files.html', files=files_w_dates)
+
+@app.route('/files/<path:filename>')
+def file(filename):
+    return flask.send_from_directory(
+        os.path.abspath(app.config['UPLOAD_FOLDER']),
+        filename,
+        as_attachment=True
+    )    
 
 if (__name__ == "__main__"):
     app.run(port = 5000)
